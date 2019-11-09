d09301d1e9a0c96f2ef49cdb2e5fa4b38400c5c7
Janis Mozgis <janismozgis@gmail.com>
2019-11-09 11:40:26 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+console.log(21); // integer (int)
+console.log(3.14); // float
+console.log(typeof 13);
+console.log(typeof 3.14);
+
+console.log('This is BOOTCAMP!!!'); // string
+console.log("This is BOOTCAMP!!!"); // string
+console.log(`This is BOOTCAMP!!!`); // string, ES6
+console.log(''); // string, empty string
+console.log(typeof 'foo');
+
+console.log(true); // bool
+console.log(false); // bool
+console.log(typeof true);
+console.log(typeof false);
+
+console.log(null); // null
+console.log(typeof null);
+
+console.log([1, 2, 3, 4, 5]); // array
+console.log(typeof [1, 2, 3, 4, 5]); // array
+console.log([1, 2, 'foo', 'bar', true, false]);
+
+// kebab-case
+// snake_case
+// camelCase
+// PascalCase
+
+var someNumber = 123;
+console.log(someNumber);
+someNumber = 321;
+console.log(someNumber);
+console.log(typeof foo);
+console.log(321);
+var someString = 'Bar';
+console.log(someString);
